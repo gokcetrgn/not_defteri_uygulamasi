1b5f21d0e2cf0512e937b773a00eb1a5e904ce56
Gokcenaz Torgan <gokcetorgan@gmail.com>
2025-02-13 16:08:29 +0300

    solved errors

--- 001-to-do-list/data/db.js
+++ 001-to-do-list/data/db.js
@@ -1,20 +1,5 @@
 const mysql = require("mysql2");
 const config = require("../config");
-,
-let connection = mysql.createConnection(config.db)
+let pool = mysql.createPool(config.db).promise()
 
-connection.connect(function(err) {
-    if(err){
-        console.log(err);
-    }
-    else{
-
-        connection.query("Select * from notes", function(err, result){
-            console.log(result);
-        })
-
-        console.log("connected to database is successfull:mysql");
-    }
-})
-
-module.exports = connection.promise;
+module.exports = pool; 
--- 001-to-do-list/server.js
+++ 001-to-do-list/server.js
@@ -1,23 +1,11 @@
 const express = require("express");
-const db = require("./data/db");
+const dbs = require("./data/db");
 
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
-
-
-const data = [
-   { id: 1, noteName : "Not 1", desc:"lorem"},
-   { id: 2 , noteName : "Not 1", desc:"lorem" },
-   { id: 3, noteName : "Not 1", desc:"lorem" },
-   { id: 4, noteName : "Not 1", desc:"lorem" },
-   { id: 5, noteName : "Not 1", desc:"lorem" },
-   { id: 6, noteName : "Not 1", desc:"lorem" },
-
-]
-
 
 
 const PORT = process.env.PORT || 3001;
@@ -29,20 +17,17 @@ app.use("/signup", function(req, res){
 });
 
 app.use("/", function(req, res){
-    db.execute("Select * from notes")
+    dbs.execute("Select * from notes")
     .then
     (
-        result => console.log(result),
+        result=> {console.log(result[0]);
         res.render("index", {
-            notes : result
-        })
-    ).
+            notes : result[0]
+        });
+    }).
     catch(err => console.log(err));
- 
-
-   
 });
-// Sunucuyu başlat
+
 app.listen(PORT, () => {
     console.log(`Sunucu ${PORT} portunda çalışıyor: http://localhost:${PORT}`);
 });
